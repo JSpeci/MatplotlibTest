3cfc42525061a063d19b92bde2bfc27051c54ce8
Your Name <you@example.com>
2018-11-12 20:35:21 +0100

    created

---
+++ MatplotlibTest/MyDataReader.cs
@@ -0,0 +1,123 @@
+﻿using DevExpress.Xpo;
+using ENVIS.Model;
+using KMB.Structures;
+using KMB.Structures.SMP;
+using KMB.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatplotlibTest
+{
+    public class MyDataReader
+    {
+        private UniRowCollection rc;
+        private IEnumerator<KmbStructure> rcen;
+        private UniDataStoreFile uds;
+        private SmpMeasNameDB record;
+        private int cntr = 0;
+        private string strInfo;
+        private Type lastType = null;
+        private List<PropertyDescEx> props;
+        private byte arch = 0;
+        private bool pressed = true;
+
+        public Dictionary<DateTime, string> values;
+
+        public MyDataReader()
+        {
+            var tag1 = (byte)SmpArchiveTypes.ARCH_MAIN;
+            var tag2 = (byte)SmpArchiveTypes.ARCH_ELMER;
+            var tag3 = (byte)SmpArchiveTypes.ARCH_PQMAIN;
+            Hyper.ComponentModel.HyperTypeDescriptionProvider.Add(typeof(XPLiteObject));
+            ENVIS.Model.DataLayerHelper.DefaultSessionInit();
+
+            values = new Dictionary<DateTime, string>();
+        }
+
+        //load cea file
+        public void LoadCea(string file)
+        {
+            if (uds != null)
+            {
+                uds.Close();//close file if some is opened
+                uds = null;
+            }
+            uds = new UniDataStoreFile(file);//cereate instance of UniDataStoreFile and initialize file
+            SmpMeasNameDB[] records = uds.OList.GetRecord((SmpObjectDB)null);//get array of all records in file
+            record = records[0];
+            LoadArchive();
+            //btnCfg.Enabled = true;
+        }
+        //load archive from datasource
+        public void LoadArchive()
+        {
+            if (uds == null) return;
+            if (rc != null) rc.Dispose();
+            var archD = new ArchDescriptionDB(arch, record.identifyDB);
+            rc = new UniRowCollection(uds, record, archD, null, null, 0, false);//create collection with archive rows
+            strInfo = archD.Name + " {0}/" + rc.Count;
+            //lblInfo.Text = string.Format(strInfo, 0);
+            rcen = rc.GetEnumerator();
+            bool n = rcen.MoveNext();
+            if (n)
+            {
+                cntr = 1;
+                lastType = null;
+                LoadNext();
+            }
+            else
+            {
+                rc.Dispose();
+            }
+        }
+
+        //read next row from archive
+        public UniArchiveBase LoadNext()
+        {
+            string str = "P_avg_3P_C";
+            UniArchiveBase row = rcen.Current as UniArchiveBase;
+            if (row == null)
+            {
+                return null;
+            }
+
+            //lblInfo.Text = string.Format(strInfo, cntr++);
+            Type t = row.GetType();
+            if (t != lastType)
+            {
+                //string str = tbProps.Text.Trim();
+                //props = PropertyDescEx.GetProperties(t, null); //get all properties of type t 
+
+                props = new List<PropertyDescEx>();
+                string[] pstr = str.Split(';');
+                PropertyDescEx prop;
+                foreach (var item in pstr)
+                {
+                    prop = PropertyDescEx.GetByName(item, t);//get property of ty by name
+                    if (prop != null) props.Add(prop);
+                }
+                ;
+                StringBuilder sb = new StringBuilder();
+
+                PropertyDescEx item2 = props[0];
+                sb.Append(row.TimeLocal.ToString() + "  ");
+                sb.Append(item2.GetName() + "\t " + item2.GetUserName(false, false) + " = " + item2.GetValue(row).ToString() + " " + item2.QuantityAtribute.unit);
+                //Console.WriteLine(sb.ToString());
+
+                if (values.TryGetValue(row.TimeLocal, out var selected) == false)
+                {
+                    values.Add(row.TimeLocal, item2.GetValue(row).ToString());
+                }
+
+                bool n = rcen.MoveNext();
+                if (!n)
+                {
+                    rc.Dispose();
+                }
+
+            }
+            return row;
+        }
+    }
+}
---
+++ MatplotlibTest/Program.cs
@@ -0,0 +1,89 @@
+﻿using MatplotlibCS;
+using MatplotlibCS.PlotItems;
+using System;
+using System.Linq;
+
+namespace MatplotlibTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = "C:\\Users\\King\\Desktop\\EnvisBackUps\\2018-04.cea";
+            MyDataReader reader = new MyDataReader();
+            reader.LoadCea(path);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                var loadedRow = reader.LoadNext();
+            }
+
+            // values restricted to one day 1.4.2018
+            var times = reader.values.Where(i => i.Key.Day == 1 && i.Key.Month == 4).Select(i => double.Parse(i.Key.Hour.ToString())).ToArray(); // 8644 values for each 10 seconds
+            var values = reader.values.Where(i => i.Key.Day == 1 && i.Key.Month == 4).Select(i => double.Parse(i.Value)).ToArray(); // 8644 values for each 10 seconds
+            var times2 = reader.values.Where(i => i.Key.Day == 1 && i.Key.Month == 4).Select(i => i.Key as object).ToArray(); // 8644 values for each 10 seconds
+
+            double[] X = new double[] { -10, -8.5, -2, 1, 6, 9, 10, 14, 15, 19 };
+            double[] Y = new double[] { -4, 6.5, -2, 3, -8, -5, 11, 4, -5, 10 };
+            string tempfolder = System.IO.Path.GetTempPath();
+
+
+            string pythonExe = "C:\\Users\\King\\AppData\\Local\\Programs\\Python\\Python37\\python.py";
+            string plotPath = "C:\\Users\\King\\source\\repos\\MatplotlibTest\\MatplotlibTest\\MatplotlibCS\\matplotlib_cs.py";
+            var matplotLibEngine = new MatplotlibCS.MatplotlibCS(pythonExe, plotPath);
+
+            var figure = new Figure(1, 1)
+            {
+                FileName = "ExampleSin.png",
+                OnlySaveImage = true,
+                DPI = 150,
+                Subplots =
+                {
+                    new Axes(1, "The X axis", "The Y axis")
+                    {
+                        Title = "Sin(x), Sin(2x), VLines, HLines, Annotations",
+                        Grid = new Grid()
+                        {
+                            MinorAlpha = 0.2,
+                            MajorAlpha = 1.0,
+                            XMajorTicks = new[] {0.0, 7.6, 0.5},
+                            YMajorTicks = new[] {-1, 2.5, 0.25},
+                            XMinorTicks = new[] {0.0, 7.25, 0.25},
+                            YMinorTicks = new[] {-1, 2.5, 0.125}
+                        },
+                        PlotItems =
+                        {
+                            new Line2D("Sin")
+                            {
+                                X = X.Select(i => i as object).ToList(),
+                                Y = Y.ToList(),
+                                LineStyle = LineStyle.Dashed
+                            },
+
+                            new Text("Text annotation","", 4.5, 0.76)
+                            {
+                                FontSize = 17
+                            },
+
+                            new Annotation("Arrow text annotation","", 0.5, -0.7, 3, 0)
+                            {
+                                Color = Color.Blue
+                            },
+
+                            new Vline("vert line", new[] {3.0 as object}, -1, 1),
+                            new Hline("hrzt line", new[] {0.1, 0.25, 0.375}, 0, 5) {LineStyle = LineStyle.Dashed, Color = Color.Magenta}
+                        }
+                    }
+
+                }
+            };
+
+            var t = matplotLibEngine.BuildFigure(figure);
+
+            t.Wait();
+
+
+            Console.ReadLine();
+        }
+    }
+}
